f063525d8b4cfa2aadfba30d14394fa176415697
jungsanghoon <gnsdl106@naver.com>
2023-12-14 18:11:57 +0900

    styled-component 추가 및 READEME 수정

---
+++ src/Components/TodoFom.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button>등록</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
---
+++ src/Components/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <ul>
+        <li>Ract학습</li>
+        <li>Typescript 학습</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
